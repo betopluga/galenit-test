32cf237a40a1d9b501b10aa98e955d267ebc675c
betopluga <carlos@plugaweb.com.br>
2020-04-29 21:05:44 -0300

    Adding cross origin to port 8082

--- backend/src/main/java/com/galenit/backend/BackendApplication.java
+++ backend/src/main/java/com/galenit/backend/BackendApplication.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jdk.nashorn.internal.ir.debug.JSONWriter;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @SpringBootApplication
 @RestController
 public class BackendApplication {
@@ -14,9 +18,10 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@CrossOrigin(origins = "http://localhost:8082")
 	@GetMapping("/hello")
 		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hi %s", name);
+		return String.format("Hello %s", name);
 	}
 
 }
